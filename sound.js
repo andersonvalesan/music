e0f777185552d7864e5213d6846a433828ccfb41
andersonvalesan <andersonvalesan@andersonvalesan>
2020-05-02 23:47:34 -0300

    add music

--- sound.js
+++ sound.js
@@ -14,15 +14,16 @@ window.onload = function (){
 	];
 
 	var audio1 = document.getElementById("audio1"); 
-	var audio2 = document.getElementById("audio2"); 
+	var flute = document.getElementById("flute"); 
 	var audio3 = document.getElementById("audio3"); 
 
 	var effects = document.getElementById("effects"); 
 
-	audio2.autoplay = true;
-	audio2.load();
+	flute.autoplay = true;
+	flute.loop = true;
+	flute.load();
 
-	for (var i = effectsName.length - 1; i >= 0; i--) {
+	for (var i = 0; i <= effectsName.length - 1; i++) {
 		var br = document.createElement("br");
 		var btn = document.createElement("BUTTON");
 		btn.setAttribute('onclick','playEffect("'+effectsName[i]+'")');
@@ -37,39 +38,39 @@ window.onload = function (){
 
 function play(e){
 	switch(e.keyCode){
-		case 38:
+		case 38: //"up arrow"
 			playEffect('gunshot.wav');
 			break;
 
-		case 65:
+		case 65://"s"
 			playEffect('tom1.wav');
 			break;
 
-		case 83:
+		case 83://"a"
 			playEffect('tom2.wav');
 			break;
 
-		case 81:
+		case 81://"q"
 			playEffect('plate.wav');
 			break;
 
-		case 80:
+		case 80://"p"
 			playEffect('doorbell.wav');
 			break;
 
-		case 77:
+		case 77://"m"
 			playEffect('msn_sound.mp3');
 			break;
 
-		case 78:
+		case 78://"n"
 			playEffect('msn_nudge.mp3');
 			break;
 
-		case 79:
+		case 79://"o"
 			playEffect('police_sirens.mp3');
 			break;
 
-		case 68:
+		case 68://"d"
 			audio3.play();
 			audio3.loop = true;
 			break;
@@ -79,6 +80,14 @@ function play(e){
 			stop(audio3);
 			break;
 	}
+}
+
+function highVolume(music){
+	document.getElementById(music).volume = 1.0;
+}
+
+function lowVolume(music){
+	document.getElementById(music).volume =  0.3;
 }
 
 function stop(audio){
@@ -93,15 +102,18 @@ function playEffect(src){
 	audio1.play();
 }
 
-function startBackgroundMusic(){
-	audio2.play();
-	audio2.loop = true;
+function startBackgroundMusic(music){
+	audio = document.getElementById(music);
+	audio.play();
+	audio.loop = true;
 }
 
-function stopBackgroundMusic(){
-	stop(audio2);
+function stopBackgroundMusic(music){
+	audio = document.getElementById(music);
+	stop(audio);
 }
 
-function pauseBackgroundMusic(){
-	audio2.pause();
+function pauseBackgroundMusic(music){
+	audio = document.getElementById(music);
+	audio.pause();
 }
